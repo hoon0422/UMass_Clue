94fde9dd7bccd169816a1c1433d2c66b51208902
Younghoon Jeong <yhjeong0422@gmail.com>
2018-03-05 18:57:18 +0900

    Add class crwaler and database updater
    Crawler is finished. It can crawl class data. Database updater is
    finished. This updater updates class data which is crawled by
    crawler.

--- lectures/models.py
+++ lectures/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Career(models.Model):
-    title = models.CharField(max_length=20, null=False)
+    title = models.CharField(max_length=15, null=False)
 
 
 class Major(models.Model):
-    title = models.CharField(max_length=20, null=False)
+    title = models.CharField(max_length=30, null=False)
 
 
 class Category(models.Model):
@@ -20,16 +20,11 @@ class YearAndSemester(models.Model):
 
 class Course(models.Model):
     title = models.CharField(max_length=50, null=False)
+    course_num = models.CharField(max_length=10, null=False)
     major = models.ForeignKey(Major, null=False, on_delete=models.DO_NOTHING)
     career = models.ForeignKey(Career, null=False, on_delete=models.DO_NOTHING)
     category = models.ForeignKey(Category, null=False, on_delete=models.DO_NOTHING)
-    course_num = models.CharField(max_length=10, null=False)
-    upper_unit = models.SmallIntegerField(null=False)
-    lower_unit = models.SmallIntegerField(null=False)
     year_and_semester = models.ForeignKey(YearAndSemester, on_delete=models.DO_NOTHING, null=False)
-
-    class Meta:
-        unique_together = (('major', 'course_num', 'year_and_semester'),)
 
 
 class Professor(models.Model):
@@ -48,7 +43,10 @@ class DayTimeField(models.Model):
 
 class Session(models.Model):
     class_num = models.CharField(max_length=5, null=False, primary_key=True)
-    professors = models.ManyToManyField(Professor, on_delete=models.DO_NOTHING)
-    building = models.ForeignKey(Building, on_delete=models.DO_NOTHING, null=False)
-    times = models.ManyToManyField(DayTimeField, on_delete=models.DO_NOTHING)
+    professors = models.ManyToManyField(Professor)
+    room = models.ForeignKey(Building, on_delete=models.DO_NOTHING, null=False)
+    times = models.ManyToManyField(DayTimeField)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False)
+    components = models.ManyToManyField(Category)
+    upper_unit = models.SmallIntegerField(null=False)
+    lower_unit = models.SmallIntegerField(null=False)
