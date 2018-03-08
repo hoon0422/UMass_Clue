Fix bug in crawler about floating numbers on units
It was found that there is a floating number on class units. So,
fields of units in session model was changed.

Also, crawler will print major index in log file and console.
Log analysis will print average and standard deviation of working
times.

web_crawler/log_analysis.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 def get_times(log_file: str):
@@ -34,4 +35,11 @@ def draw_time_diff_graph(log_file):
 
 
 if __name__ == "__main__":
-    draw_time_diff_graph("../raw/log.txt")
+    time_diff = get_time_diff(get_times("../raw/log.txt"))
+    print("Average:", np.mean(time_diff))
+    print("STD:", np.std(time_diff))
+
+    plt.ylabel('Time difference (sec)')
+    plt.xlabel('Number of data')
+    plt.plot(time_diff)
+    plt.show()