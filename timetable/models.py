8084795c65b1eaca992acafaebb87ef19bbb8e4d
Younghoon Jeong <yhjeong0422@gmail.com>
2018-03-30 23:26:24 +0900

    Only authenticated user can use timetable
    1. From now on, only authenticated user can use their timetable.
    
    2. User can search lectures in timetable page.

--- timetable/models.py
+++ timetable/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from lectures.models import Section
+from accounts.models import User
 
 
 class Timetable(models.Model):
     title = models.CharField(max_length=20, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
     sections = models.ManyToManyField(Section)
 
     def __str__(self):
--- timetable/views.py
+++ timetable/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
+from lectures.forms import SearchForm
 
 
-class TimetableView(generic.TemplateView):
+class TimetableView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'timetable/timetable.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['timetables'] = models.Timetable.objects.all()
+        context["search_form"] = SearchForm(self.request.GET, self.request.FILES)
+        context['timetables'] = models.Timetable.objects.filter(user=self.request.user)
         return context
