f543dfcba655dffd7921f4b48b2f1cbb76214353
Younghoon Jeong <yhjeong0422@gmail.com>
2018-04-08 21:15:26 +0900

    Add signals for constructor and destructor of 'Timetable' instance
    To make a newly added timetable as the default timetable for display,
    add a signal for constructor of 'Timetable' instance. Also, to prevent
    a situation that there are no default timetable when the default
    timetable is deleted, add a signal for destructor of 'Timetable'
    instance. To implement the destructor, 'modified_date' field is added,
    which means the date when a timetable is modified.
    
    1. set_new_timetable_as_default(sender, instance, **kwargs)
    Call 'set_as_default' method of the timetable instance which is newly
    added.
    
    2. set_recent_timetable_as_default_when_default_deleted(
        sender, instance, **kwargs)
    When the default timetable is deleted, the recently modified timetable
    will be the default timetable of the user.
    
    3. modified_date
    A field of Timetable model which shows date and time of modification.

--- timetable/models.py
+++ timetable/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from lectures.models import Section
 from accounts.models import User
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
 
 
 class Timetable(models.Model):
@@ -8,6 +10,7 @@ class Timetable(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
     sections = models.ManyToManyField(Section)
     _default = models.BooleanField(null=False, default=False)
+    modified_date = models.DateTimeField(auto_now=True, auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -32,3 +35,25 @@ class Timetable(models.Model):
             current_default._default = False
 
         self._default = True
+
+
+@receiver(post_save, sender=Timetable)
+def set_new_timetable_as_default(sender, instance, **kwargs):
+    instance.set_as_default()
+
+
+@receiver(pre_delete, sender=Timetable)
+def set_recent_timetable_as_default_when_default_deleted(sender, instance, **kwargs):
+    if len(Timetable.objects.filter(user=instance.user)) <= 1:
+        return
+
+    if instance.default is False:
+        return
+
+    recents = Timetable.objects.filter(user=instance.user).order_by('-modified_date')[:2]
+    if recents[0] is instance:
+        recent = recents[1]
+    else:
+        recent = recents[0]
+
+    recent.set_as_default()
