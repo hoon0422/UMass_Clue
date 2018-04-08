c164dbb9aa206ed205e18f6e3260fb620fc7dd68
Younghoon Jeong <yhjeong0422@gmail.com>
2018-04-08 20:19:13 +0900

    Add 'default' attribute in 'Timetable' model
    'default' attribute means a timetable is a timetable that users will see
    at first when they click their page for timetables. Only one timetable
    can be the default timetable among users' timetables. To implement this,
    some methods are also added.
    
    The followings are the added methods.
    
    1. default()
    Getter method of 'default'.
    
    2. default(value)
    Setter method of 'default'. However, this will raise AttributeError
    because it can harm the meaning of 'default' attribute, which means
    there must be only one default timetable.
    
    3. set_as_default()
    This method is kind of setter of 'default' attribute. It sets 'default'
    attribute of the current default timetable as false, and sets the
    attribute of the new default timetable as true.

--- timetable/models.py
+++ timetable/models.py
@@ -7,6 +7,28 @@ class Timetable(models.Model):
     title = models.CharField(max_length=20, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
     sections = models.ManyToManyField(Section)
+    _default = models.BooleanField(null=False, default=False)
 
     def __str__(self):
         return self.title
+
+    @property
+    def default(self):
+        return self._default
+
+    @default.setter
+    def default(self, value):
+        raise AttributeError("You cannot set 'default' attribute directly")
+
+    def set_as_default(self):
+        current_default = None
+        user_timetables = Timetable.objects.filter(user=self.user)
+        for i in range(len(user_timetables)):
+            if user_timetables[i].default:
+                current_default = user_timetables[i]
+                break
+
+        if current_default is not None:
+            current_default._default = False
+
+        self._default = True
