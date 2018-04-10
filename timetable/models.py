99d6a1e6cfa5024eeb08cfeb7da23387c0dfbe6c
Younghoon Jeong <yhjeong0422@gmail.com>
2018-04-10 16:59:53 +0900

    Fix bug about saving which timetable is the default timetable
    There was a bug that data about which timetable is the default timetable
    in database was not saved. Changing when to send a signal and using
    proper functions to update data, bug is fixed.

--- timetable/models.py
+++ timetable/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from lectures.models import Section
 from accounts.models import User
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 
@@ -27,14 +27,16 @@ class Timetable(models.Model):
         current_default = None
         user_timetables = Timetable.objects.filter(user=self.user)
         for i in range(len(user_timetables)):
-            if user_timetables[i].default:
+            if user_timetables[i].default and user_timetables[i] is not self:
                 current_default = user_timetables[i]
                 break
 
         if current_default is not None:
-            current_default._default = False
+            Timetable.objects.filter(id=current_default.id).update(_default=False)
+            current_default.refresh_from_db()
 
-        self._default = True
+        Timetable.objects.filter(id=self.id).update(_default=True)
+        self.refresh_from_db()
 
 
 @receiver(post_save, sender=Timetable)
@@ -42,18 +44,14 @@ def set_new_timetable_as_default(sender, instance, **kwargs):
     instance.set_as_default()
 
 
-@receiver(pre_delete, sender=Timetable)
+@receiver(post_delete, sender=Timetable)
 def set_recent_timetable_as_default_when_default_deleted(sender, instance, **kwargs):
-    if len(Timetable.objects.filter(user=instance.user)) <= 1:
+    if len(Timetable.objects.filter(user=instance.user)) == 0:
         return
 
     if instance.default is False:
         return
 
-    recents = Timetable.objects.filter(user=instance.user).order_by('-modified_date')[:2]
-    if recents[0] is instance:
-        recent = recents[1]
-    else:
-        recent = recents[0]
-
+    recent = Timetable.objects.filter(user=instance.user).order_by('-modified_date')[0]
+    print(recent)
     recent.set_as_default()
